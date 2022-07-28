1b488afc05713bf90e132978f24c37ed4b258646
Dmitri Kuznetsov <kna_int@mail.ru>
2022-07-28 21:06:24 +0300

    Added weapon and projectile

---
+++ scenes/weapon/BasicGun.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class BasicGun : Node
+{
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+}
