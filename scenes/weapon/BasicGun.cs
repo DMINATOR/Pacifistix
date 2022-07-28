cab7a6d83b1f468e9e2d14ffc7f37e31ec965d99
Dmitri Kuznetsov <kna_int@mail.ru>
2022-07-28 22:23:54 +0300

    spawning projectiles as expected

--- scenes/weapon/BasicGun.cs
+++ scenes/weapon/BasicGun.cs
@@ -6,22 +6,29 @@ public class BasicGun : Node2D
     [Export]
     public PackedScene ProjectileScene;
 
-    private Line2D _projectileDirection;
+    [Export]
+    public float ProjectileSpeed = 100; // How fast the projectile will move (pixels/sec).
+
+    private Position2D _spawnLocation;
 
     public override void _Ready()
     {
-        _projectileDirection = GetNode<Line2D>("ProjectileDirection");
+        _spawnLocation = GetNode<Position2D>("SpawnLocation");
     }
 
     public void ShootProjectile()
     {
         var newProjectile = ProjectileScene.Instance<BulletProjectile>();
 
-        newProjectile.Position = _projectileDirection.Points[0];
-        var direction = _projectileDirection.Points[1];
-        newProjectile.LinearVelocity = direction;
+        //newProjectile.Position = _projectileDirection.Points[0];
+        //var direction = _projectileDirection.Points[1];
 
-        // Add child
-        this.AddChild(newProjectile);
+        //newProjectile.LinearVelocity = newProjectile.Position - direction;
+
+        newProjectile.Transform = _spawnLocation.GlobalTransform;
+        newProjectile.LinearVelocity = new Vector2(0, -ProjectileSpeed).Rotated(this.Rotation);
+
+        // Add child to the root
+        Owner.Owner.AddChild(newProjectile);
     }
 }
