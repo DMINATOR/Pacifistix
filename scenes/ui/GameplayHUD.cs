a6f4db14aab5698a6ef1df990bb9615128525f26
Dmitri Kuznetsov <kna_int@mail.ru>
2022-08-02 22:19:22 +0300

    Gun shot effect created

--- scenes/ui/GameplayHUD.cs
+++ scenes/ui/GameplayHUD.cs
@@ -3,14 +3,16 @@ using System;
 
 public class GameplayHUD : Control
 {
+    private PlayerGunsHUD _playerGunsHUD;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _playerGunsHUD = GetNode<PlayerGunsHUD>("PlayerGunsHUD");
     }
 
     public void OnPlayerGunShootDelegateCallback(BaseGun gun)
     {
-        GD.Print("shot");
+        _playerGunsHUD.GunShotTweenStart(gun);
     }
 }
---
+++ scenes/ui/PlayerGunsHUD.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public class PlayerGunsHUD : Control
+{
+    // Color at the start of the shot effect
+    [Export]
+    public Color GunShotEffectColorStart;
+
+    // Color at the end of of the shot effect
+    [Export]
+    public Color GunShotEffectColorEnd;
+
+    // Duration of the gun shot effect
+    [Export]
+    public float GunShotEffectDuration;
+
+    // Node to update with shot effect
+    private ColorRect _colorRect;
+
+    private Tween _gunShotTween;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _colorRect = GetNode<ColorRect>("GunShotEffect/ColorRect");
+
+        _gunShotTween = GetNode<Tween>("GunShotEffect/GunShotTween");
+    }
+
+    // Trigger tween event to show 
+    public void GunShotTweenStart(BaseGun gun)
+    {
+        _gunShotTween.InterpolateProperty(
+            _colorRect,
+            "color",
+            GunShotEffectColorStart,
+            GunShotEffectColorEnd,
+            GunShotEffectDuration);
+
+        _gunShotTween.Start();
+    }
+
+    public void GunShotTweenEnd()
+    {
+
+    }
+}
