7037ce6ea7c6b84b63ba27ae9f344f46f7d80693
Dmitri Kuznetsov <kna_int@mail.ru>
2022-08-04 22:07:56 +0300

    Added additional weapon

--- scenes/weapon/BurstShotGun.cs
+++ scenes/weapon/BurstShotGun.cs
@@ -9,6 +9,7 @@ public class BurstShotGun : BaseGun
     [Export]
     public float ProjectileSpeed = 100; // How fast the projectile will move (pixels/sec).
 
+
     private Position2D _spawnLocation;
 
     public override void _Ready()
